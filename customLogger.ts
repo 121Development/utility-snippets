e13a23617b120a9bcc8f744022fe573c4887cc98
121Development <67238945+121Development@users.noreply.github.com>
2021-12-16 08:50:59 +0100

    refactor for TS 2

--- customLogger.ts
+++ customLogger.ts
@@ -10,24 +10,21 @@ try{
     let lineNr = caller_line.slice(caller_line.indexOf(':')+1, caller_line.lastIndexOf(':')).trim()
     if(fnName.substring(0,1) == '/') { fnName = 'IFFE'}
     console.log(`[log] ${fnName} in ${fileName} @ line: ${lineNr} ${msg} ${variable}`)
-} catch(e){ console.log(e ) }
+} catch(e){ console.log(e) }
 }
 
 // regular and arrow function examples
 function foo(): void{ log("test of message parameter with variable as a number", 3) }
-const  bar = () =>{ log() }
+const bar = () =>{ log() }
 
 foo()
 bar()
 
 // IFFE functions examples
 let IFFE = ( () =>{ log() })();
+
 (() =>{ log() })();
 
 
-// Other method of logging function name
-function log2(): void{ console.log(arguments.callee.name) }
-
-//log2()
 
 
